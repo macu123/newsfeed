e2f37436bf997edb4b159d2c44faa1ae8ff195f0
John Chen <chenjiapeng12345@gmail.com>
2018-04-17 19:00:30 -0400

    add feeds index page

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,4 +2,22 @@ class Feed < ActiveRecord::Base
   has_many :comments
 
   paginates_per 30
+
+  def displayed_website
+    return nil unless self.url
+    
+    arr = self.url.split('//')
+    if arr.size > 1
+      website = arr[1]
+    else
+      website = arr[0]
+    end
+    website = website.split('/')[0]
+    website.sub!('www.', '') if website.starts_with?('www')
+  end
+
+  def displayed_time
+    num_hours = ((Time.now - self.created_at) / 3600).round
+    "#{num_hours} hours ago"
+  end
 end
